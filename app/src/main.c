fce27a4284dfdbaa1cf6fc5f72359964d4db5800
adakPaleczny <adampaleczny02@gmail.com>
2026-01-17 14:41:22 +0100

    start on button pressed

--- app/src/main.c
+++ app/src/main.c
@@ -23,9 +23,13 @@
 #include <zephyr/sys/__assert.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/input/input.h>
+#include <zephyr/sys/atomic.h>
 
 #include <lvgl.h>
 #include <font/lv_font.h>
+#include <lvgl_mem.h>
+#include <lvgl_zephyr.h>
 
 #include <stdarg.h>
 
@@ -86,6 +90,29 @@ static int img_with_gt = 0;
 static int img_with_pred = 0;
 static int gt_matched = 0;
 static int pred_matched = 0;
+
+static int bench_total = 0;
+static int bench_done = 0;
+static int bench_errors = 0;
+
+static int bench_last_ms = 0;
+static int bench_min_ms = 0x7fffffff;
+static int bench_max_ms = 0;
+static int64_t bench_sum_ms = 0;
+static int bench_samples = 0;
+
+/* Touch sample container used by your callback */
+struct touch_point_t
+{
+	int32_t x;
+	int32_t y;
+	bool pressed;
+};
+
+static struct touch_point_t touch_point;
+
+/* Semaphore signaled on evt->sync */
+K_SEM_DEFINE(sync, 0, 1);
 
 static inline struct box_xyxy xywh_to_xyxy(float x, float y, float w, float h)
 {
@@ -361,6 +388,96 @@ static void decorate_canvas(lv_obj_t *canvas)
 	lv_canvas_finish_layer(canvas, &layer);
 }
 
+static void draw_progress_bar(lv_layer_t *layer, int x, int y, int w, int h, float percent)
+{
+	lv_draw_rect_dsc_t box, fill;
+	lv_area_t a;
+
+	if (percent < 0)
+		percent = 0;
+	if (percent > 100)
+		percent = 100;
+
+	lv_draw_rect_dsc_init(&box);
+	box.border_width = 2;
+	box.border_color = lv_color_white();
+	box.radius = 6;
+	box.bg_opa = LV_OPA_10;
+
+	a.x1 = x;
+	a.y1 = y;
+	a.x2 = x + w;
+	a.y2 = y + h;
+	lv_draw_rect(layer, &box, &a);
+
+	lv_draw_rect_dsc_init(&fill);
+	fill.radius = 6;
+	fill.bg_opa = LV_OPA_COVER;
+	fill.bg_color = lv_palette_main(LV_PALETTE_BLUE);
+	fill.border_width = 0;
+
+	int fw = (int)((w - 4) * (percent / 100.0f));
+	if (fw < 0)
+		fw = 0;
+	a.x1 = x + 2;
+	a.y1 = y + 2;
+	a.x2 = x + 2 + fw;
+	a.y2 = y + h - 2;
+	if (a.x2 > a.x1)
+	{
+		lv_draw_rect(layer, &fill, &a);
+	}
+}
+
+static void decorate_benchmark(lv_obj_t *canvas, float percent)
+{
+	const lv_font_t *font = &lv_font_unscii_16;
+	lv_layer_t layer;
+	uint32_t ver = sys_kernel_version_get();
+	int load = 0;
+
+	lv_canvas_init_layer(canvas, &layer);
+
+	print_text(&layer, 0, 0, LV_TEXT_ALIGN_CENTER, "Benchmark: YOLOv2");
+	print_text(&layer, 0, font->line_height, LV_TEXT_ALIGN_CENTER,
+			   "Zephyr %d.%d.%d", SYS_KERNEL_VER_MAJOR(ver), SYS_KERNEL_VER_MINOR(ver), SYS_KERNEL_VER_PATCHLEVEL(ver));
+
+	/* Progress bar */
+	int bar_w = DISPLAY_WIDTH - 40;
+	int bar_h = 18;
+	int bar_x = 20;
+	int bar_y = font->line_height * 3;
+	draw_progress_bar(&layer, bar_x, bar_y, bar_w, bar_h, percent);
+
+	print_text(&layer, 0, bar_y + bar_h + 4, LV_TEXT_ALIGN_CENTER,
+			   "%d / %d (%.1f%%)", bench_done, bench_total, (double)percent);
+
+	/* Latency stats */
+	int avg = (bench_samples > 0) ? (int)(bench_sum_ms / bench_samples) : 0;
+	int fps = (bench_last_ms > 0) ? (1000 / bench_last_ms) : 0;
+
+	int y0 = DISPLAY_HEIGHT - font->line_height * 6;
+	print_text(&layer, 0, y0 + font->line_height * 0, LV_TEXT_ALIGN_LEFT,
+			   "Last: %d ms  (%d FPS)", bench_last_ms, fps);
+	print_text(&layer, 0, y0 + font->line_height * 1, LV_TEXT_ALIGN_LEFT,
+			   "Avg : %d ms", avg);
+	print_text(&layer, 0, y0 + font->line_height * 2, LV_TEXT_ALIGN_LEFT,
+			   "Min : %d ms   Max: %d ms", (bench_min_ms == 0x7fffffff) ? 0 : bench_min_ms, bench_max_ms);
+
+#ifdef CONFIG_CPU_LOAD
+	load = cpu_load_get(1);
+	print_text(&layer, 0, y0 + font->line_height * 3, LV_TEXT_ALIGN_LEFT,
+			   "CPU : %d.%d%%", load / 10, load % 10);
+#endif
+
+	/* Your existing detection benchmark counters */
+	print_text(&layer, 0, y0 + font->line_height * 4, LV_TEXT_ALIGN_LEFT,
+			   "GT:%d  Pred:%d  MatchGT:%d  MatchPred:%d",
+			   img_with_gt, img_with_pred, gt_matched, pred_matched);
+
+	lv_canvas_finish_layer(canvas, &layer);
+}
+
 static int video_crop_setup(const struct device *const video_dev, uint32_t sensor_width, uint32_t sensor_height)
 {
 	const float ratioy = (float)sensor_height / DISPLAY_HEIGHT;
@@ -500,12 +617,13 @@ static int video_setup(const struct device *const main_dev, const struct device 
 // /* --- Simple RGB565 framebuffer for the LVGL canvas --- */
 // static uint16_t jpg_fb[DISPLAY_WIDTH * DISPLAY_HEIGHT];
 
-static int ls_dir(const char *path)
+static int ls_dir(const char *path, lv_obj_t *canvas)
 {
 	int res;
 	struct fs_dir_t dirp;
 	static struct fs_dirent entry;
 	int count = 0;
+	bench_total = 1500; // or 3000, whatever your target is
 
 	fs_dir_t_init(&dirp);
 
@@ -614,6 +732,23 @@ static int ls_dir(const char *path)
 			}
 		}
 		count++;
+		bench_last_ms = model_get_latest_inference_time();
+		if (bench_last_ms > 0)
+		{
+			bench_min_ms = MIN(bench_min_ms, bench_last_ms);
+			bench_max_ms = MAX(bench_max_ms, bench_last_ms);
+			bench_sum_ms += bench_last_ms;
+			bench_samples++;
+		}
+		else
+		{
+			bench_errors++;
+		}
+		bench_done = count;
+
+		decorate_benchmark(canvas, count / bench_total * 1000);
+		lv_timer_handler();
+
 		if (count > 1500)
 		{
 			LOG_INF("END OF BENCHMARK");
@@ -650,6 +785,28 @@ static int ls_dir(const char *path)
 
 	return res;
 }
+static bool pressed;
+
+static void button_input_cb(struct input_event *evt, void *user_data)
+{
+	if (evt->sync == 0)
+	{
+		return;
+	}
+
+	// printk("Button %d %s at %" PRIu32 "\n",
+	// 	   evt->code,
+	// 	   evt->value ? "pressed" : "released",
+	// 	   k_cycle_get_32());
+
+	if (evt->value == 0)
+	{
+		LOG_INF("Button released - Benchmark started!!!!");
+		pressed = 1;
+	}
+}
+
+INPUT_CALLBACK_DEFINE(NULL, button_input_cb, NULL);
 
 int main()
 {
@@ -657,13 +814,14 @@ int main()
 	const struct device *const display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	// const struct device *const video_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_camera));
 	// struct video_buffer *vbuf;
-	// lv_obj_t *canvas;
+	lv_obj_t *canvas;
 	k_tid_t nn_tid;
 	int ret;
 
 	// __ASSERT_NO_MSG(device_is_ready(video_dev));
 	// __ASSERT_NO_MSG(device_is_ready(camera_aux_dev));
 	__ASSERT_NO_MSG(device_is_ready(display_dev));
+	__ASSERT_NO_MSG(device_is_ready(touch_dev));
 
 	// /* move main thread priority to lowest one so we let others thread a chance to run */
 	// k_thread_priority_set(k_current_get(), K_LOWEST_APPLICATION_THREAD_PRIO);
@@ -696,18 +854,24 @@ int main()
 
 	// LOG_INF("STARTING");
 
-	// canvas = lv_canvas_create(lv_scr_act());
+	canvas = lv_canvas_create(lv_scr_act());
 	// while (1) {
 	// 	ret = video_dequeue(video_dev, &vbuf, K_FOREVER);
 	// 	__ASSERT_NO_MSG(ret == 0);
 
 	// 	lv_canvas_set_buffer(canvas, vbuf->buffer, DISPLAY_WIDTH, DISPLAY_HEIGHT, LV_COLOR_FORMAT_RGB565);
-	// 	decorate_canvas(canvas);
-	// 	lv_timer_handler();
 
 	// 	ret = video_enqueue(video_dev, vbuf);
 	// 	__ASSERT_NO_MSG(ret == 0);
 	// }
+
+	while (!pressed)
+	{
+		lv_timer_handler();
+		k_sleep(K_MSEC(10));
+	}
+
+	// ls_dir(dir_path, canvas);
 
 	static const char *disk_mount_pt = DISK_MOUNT_PT;
 	mp.mnt_point = disk_mount_pt;
@@ -724,7 +888,7 @@ int main()
 	}
 	const char *dir_path = "/SD:/BIN_224";
 
-	ls_dir(dir_path);
+	ls_dir(dir_path, canvas);
 
 	/* Run slideshow: 1000 ms per image */
 	// run_slideshow(img, dir_path, 1000);
